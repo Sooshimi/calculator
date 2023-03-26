6c2e63350d105b26951ad6bda77e8117db2644ce
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-03-26 22:20:43 +0100

    Handle divisions by 0

--- script.js
+++ script.js
@@ -10,7 +10,12 @@ const numDisplay = document.querySelector(".display");
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+const divide = (a, b) => {
+    if (b === 0) {
+        return "Error";
+    }
+    a / b;
+};
 const modulo = (a, b) => a % b;
 
 function operate(a, b, operator) {
